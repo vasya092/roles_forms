5fb4733bdda56ad66d2173082150adbcd497d8f5
vasya092 <vasya092@gmail.com>
2020-03-14 15:28:27 +0500

    Fix query

--- db/get_user_table.php
+++ db/get_user_table.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'db.php';
 
-$query = DB::run("SELECT user.username, user_role.rolename FROM user INNER JOIN user_role ON user.id = user_role.id;");
+$query = DB::run("SELECT user.username, user_role.rolename FROM user INNER JOIN user_role ON user.role_id = user_role.id;");
 $data = array();
 while ($row = $query->fetch(PDO::FETCH_OBJ))
 {
